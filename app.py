c2af1c5f7b8fd35cf962cf65ed596791cd0d30ca
ubaid_patel <fakeuser595@gmail.com>
2023-09-14 21:23:08 +0530

    First commit app is working fine

---
+++ app.py
@@ -0,0 +1,92 @@
+from flask import Flask,send_file,make_response,render_template
+from pytube import YouTube,Stream
+import math
+from flask import request
+from moviepy.editor import VideoFileClip, AudioFileClip
+from pydub import AudioSegment
+import os,time,threading
+from flask_cors import CORS
+def performCleanup(file,t):
+    time.sleep(t)
+    os.remove(file)
+
+
+app = Flask("Yt Downloader")
+CORS(app,supports_credentials=True,resources={r"/*": {"origins":"*"}},allow_headers=['*','Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, filename'])
+@app.route("/")
+def home():
+   return render_template("index.html")
+
+@app.route("/info",methods=["POST"])
+def info():
+    print(request.get_json()["link"])
+    yt = YouTube(request.get_json()["link"])  
+    options = []  
+    
+    for stream in yt.streams.filter(progressive=False):
+     if(stream.mime_type == "video/mp4"):
+       options.insert(len(options),{"itag":stream.itag,"type":stream.type,"quality":stream.resolution,"size":math.ceil(stream.filesize / 1024 / 1024)})
+     elif(stream.mime_type == "audio/mp4"):
+       options.insert(len(options),{"itag":stream.itag,"type":stream.type,"quality":stream.abr,"size":math.ceil(stream.filesize / 1024 / 1024)})
+       
+    response = {"thumbnail":yt.thumbnail_url,"title":yt.title,"options":options}
+    return response
+    
+@app.route("/download",methods=['POST'])
+def download():  
+    yt = YouTube(request.get_json()["link"])  
+    file = yt.streams.get_by_itag(request.get_json()["itag"])
+    headers = {
+        'Content-Disposition': f'attachment; filename={file.default_filename.split("/")[-1]}',
+        'Content-Type': 'application/octet-stream',
+        'filename':file.default_filename
+         }
+    if file.mime_type == "video/mp4":
+       fname = file.default_filename.encode('ascii', 'ignore').decode('ascii')
+       file.download(filename="1"+fname)
+       audio = yt.streams.filter(only_audio=True,file_extension="mp4",bitrate="128kbps").first()
+       audio.download(filename=fname.replace(".mp4",".m4a"))
+       video_clip = VideoFileClip(filename="1"+fname)  # Load the video
+       audio_clip = AudioFileClip(filename=fname.replace(".mp4",".m4a"))
+
+       # Ensure the audio duration matches the video duration
+       if audio_clip.duration > video_clip.duration:
+           audio_clip = audio_clip.subclip(0, video_clip.duration)
+
+       # Set the audio of the video clip to the audio clip
+       video_clip = video_clip.set_audio(audio_clip)
+
+       # Specify the frame rate here (e.g., 30 FPS)
+       video_clip = video_clip.set_duration(video_clip.duration)
+
+       # Write the merged video to an output file
+       video_clip.write_videofile(fname)
+       print("Video Converted")
+       os.remove(fname.replace(".mp4",".m4a"))
+       os.remove("1"+fname)
+       threading.Thread(target=performCleanup,args=(fname,300)).start()
+    else:
+        filename = file.default_filename.replace(".mp4",".m4a").encode('ascii', 'ignore').decode('ascii')
+        file.download(filename=filename)
+        audio_clip = AudioFileClip(filename)
+        # Export the audio to MP3 format
+        audio_clip.write_audiofile(filename.replace(".m4a",".mp3"), codec="mp3")
+        os.remove(filename)
+        
+        print("Audio converted")
+        headers['Content-Disposition'] = f'attachment; filename={filename.replace(".m4a",".mp3").split("/")[-1]}'
+        headers['filename'] = filename.replace(".m4a",".mp3")
+        threading.Thread(target=performCleanup,args=(filename.replace(".m4a",".mp3"),300)).start()
+    
+    response = make_response(send_file(headers['filename'], as_attachment=True, download_name=headers['filename']))
+    response.headers = headers
+    return response
+
+
+@app.route("/t/<a>")
+def test( ):
+   return "a"
+if __name__ == "__main__":
+    app.run(debug=True,host='192.168.43.160',port=5000)
+
+ 
---
+++ templates/readme.md
@@ -0,0 +1,46 @@
+# Clip Catcher - YouTube Downloader
+
+Clip Catcher is a user-friendly YouTube downloader application that allows you to easily download YouTube videos for offline viewing. Our app is free, ad-free, and doesn't use any redirects. Download your favorite YouTube videos hassle-free with Clip Catcher.
+
+## Features
+
+- Download YouTube videos in various formats and resolutions.
+- No ads, no redirects, and completely free to use.
+- Simple and intuitive user interface.
+- Fast and efficient video downloads.
+- Supports downloading videos with captions.
+- Cross-platform compatibility (Windows, macOS, Linux).
+
+## How to Use
+
+1. **Paste Video URL**: Copy the URL of the YouTube video you want to download.
+2. **Open Clip Catcher**: Launch the Clip Catcher app.
+3. **Paste URL**: Paste the copied video URL into Clip Catcher's input field.
+4. **Choose Format and Resolution**: Select your preferred video format and resolution.
+5. **Start Download**: Click the "Download" button to start the download.
+6. **Enjoy Offline Viewing**: Once the download is complete, enjoy your video offline.
+
+## Installation
+
+Clip Catcher can be installed on Windows, macOS, and Linux. Follow the installation instructions for your specific operating system in our [Installation Guide](installation.md).
+
+## Frequently Asked Questions
+
+Have questions? Check out our [FAQ](faq.md) for answers to common queries.
+
+## Reporting Issues
+
+If you encounter any issues or have suggestions for improving Clip Catcher, please [report them here](https://github.com/your-username/clip-catcher/issues).
+
+## License
+
+Clip Catcher is open-source software released under the [MIT License](LICENSE).
+
+## Contact Us
+
+- Email: support@clipcatcherapp.com
+- Website: [www.clipcatcherapp.com](https://www.clipcatcherapp.com)
+
+We appreciate your support and hope you enjoy using Clip Catcher!
+
+Happy downloading!
